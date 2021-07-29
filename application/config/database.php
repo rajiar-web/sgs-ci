448281c494e5d4fab98668e6aaed62cb2dd0201f
rajiar-web <67356815+rajiar-web@users.noreply.github.com>
2021-07-29 10:47:23 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,13 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+// 	'hostname' => '160.153.221.36',
+// 	'username' => 'admsgs',
+// 	'password' => '0J-S*=1gZZ(g',
+// 	'database' => 'db_sgs_new',
+	'hostname' => 'mysql-41283-0.cloudclusters.net:19430',
+	'username' => 'admin',
+	'password' => 'SZ1ITvPb',
 	'database' => 'db_sgs',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
